02e76b4bf787bcc10223a6a08460659479288fc2
nadir.akdag <nadir.akdag@getir.com>
2023-04-13 12:33:07 +0300

    added database integration

---
+++ internal/db/postgres/db.go
@@ -0,0 +1,28 @@
+package postgres
+
+import (
+	"comment-service/config"
+	"context"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	Client *sqlx.DB
+}
+
+func NewDatabase(config config.Database) (*Database, error) {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%v", config.Host, config.Port, config.Username, config.Database, config.Password, config.SslMode)
+
+	dbConnection, err := sqlx.Connect("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{Client: dbConnection}, nil
+}
+
+func (d *Database) Ping(ctx context.Context) error {
+	return d.Client.PingContext(ctx)
+}
